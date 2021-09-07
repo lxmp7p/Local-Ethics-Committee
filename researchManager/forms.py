92b4380b8ee4f5b7cd614d9d98ba3e8bdffb8d00
Zaur <lxmp7p@yandex.ru>
2021-09-07 16:39:23 +0300

    Added load preclinical research

--- researchManager/forms.py
+++ researchManager/forms.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+
 from django import forms
 
 from .models import Information
@@ -11,7 +13,16 @@ class ClinicalResearchInformationForm(forms.ModelForm):
         model = Information
         fields = (
             "protocol_name", "drug_name", "main_researcher", "research_center",
-            "customer", "customer_contacts", "duration", "research", "protocol_number"
+            "customer", "customer_contacts", "duration", "research", "protocol_number", "research"
+        )
+
+
+class PreclinicalResearchInformationForm(forms.ModelForm):
+    """Форма клинического исследования"""
+    class Meta:
+        model = Information
+        fields = (
+            "work_name", "division", "executor", 'research'
         )
 
 
--- researchManager/functions/loadResearch.py
+++ researchManager/functions/loadResearch.py
@@ -1,41 +1,65 @@
 import folder as folder
 from django.core.files.storage import FileSystemStorage
 
-from ..forms import ClinicalResearchInformationForm
-from ..models import Researh, Files
+from ..forms import ClinicalResearchInformationForm, PreclinicalResearchInformationForm
+from ..models import Researh, Files, Information
 import datetime
 import re
 
 now = datetime.datetime.now()
 
 def AddResearch(request=None, researchType=None, requestType=None):
-    """Добавление исследования"""
-    if requestType == "firstRequest":
-        researchId = addResearchMainData(request, researchType, requestType)
-        if researchType == "clinicalResearch":
-            protocol_number = AddClinicalResearch(request, researchId)
-    saveFiles(request.FILES, request.POST, protocol_number, researchId)
+    """
+    - Добавление исследования
+    Для добавления нового типа исследования нужно:
+        * Создать if с проверкой типа исследования (if researchType == 'Тип исследования в БД')
+        * Вызвать функицию с добавлением информации в бд и записать значение в protocol_number
+        (protocol_number = AddClinicalResearch(request, researchId))
+    """
+    researchId = addResearchMainData(researchType, requestType)
+    folderName = AddClinicalResearch(request, researchId, researchType)
+    saveFiles(request.FILES, request.POST, folderName, researchId)
 
 
-def addResearchMainData(request, researchType, requestType):
-    """Добавление служеюных данных исследования"""
+def addResearchMainData(researchType, requestType):
+    """Добавление служебных данных исследования"""
     Researh.objects.create(type_request=requestType, identityCode='rand', version='1', owner='lxmp7p', type=researchType)
     research = Researh.objects.all().last()
     return research.id
 
-
-def AddClinicalResearch(request, idResearch):
-    """Добавление клинического исследования"""
-    protocol_number = None
-    informationForm = ClinicalResearchInformationForm(request.POST)
+def AddClinicalResearch(request, idResearch, researchType):
+    """
+    Добавление клинического исследования
+    * Оптимизировать и уменьшить IF
+    """
+    folderName = None
+    if researchType == "clinicalResearch":
+        informationForm = ClinicalResearchInformationForm(request.POST)
+    if researchType == "preclinicalResearch":
+        informationForm = PreclinicalResearchInformationForm(request.POST)
     if informationForm.is_valid():
         informationForm = informationForm.save(commit=False)
-        protocol_number = informationForm.protocol_number
+        if researchType == "clinicalResearch":
+            folderName = informationForm.protocol_number
+        if researchType == "preclinicalResearch":
+            folderName = informationForm.work_name
+        informationForm.research_id = idResearch
+        informationForm.save()
+    return folderName
+
+def AddPreclinicalResearch(request, idResearch):
+    """Добавление доклинического исследования"""
+    work_name = None
+    informationForm = PreclinicalResearchInformationForm(request.POST)
+    if informationForm.is_valid():
+        informationForm = informationForm.save(commit=False)
+
         informationForm.info_research_id = idResearch
         informationForm.save()
-    return protocol_number
+    return work_name
 
 def getFileInfo(filesInfo, file):
+    """Выдает информацию и пути к файлу, а так же название документа, версию и его дату"""
     date, version, name = '', '', ''
     if filesInfo.get(file+'_date'):
         date = filesInfo.get(file+'_date')
@@ -45,9 +69,9 @@ def getFileInfo(filesInfo, file):
         name = filesInfo.get(file+'name')
     return date, version, name
 
-def saveFiles(files, filesInfo, protocol_number, researchId):
+def saveFiles(files, filesInfo, folderName, researchId):
     """Сохранение файлов и запись в БД информации о них"""
-    folder_name = f'/{str(now.strftime("%Y"))}/{protocol_number}/'
+    folder_name = f'/{str(now.strftime("%Y"))}/{folderName}/'
     fs = FileSystemStorage()
     fs.base_location = fs.base_location + folder_name
     for file in files:
@@ -64,5 +88,3 @@ def saveFiles(files, filesInfo, protocol_number, researchId):
                 file=fileUrl, research_id=researchId,
                 date=date, version=version, name=name
             )
-
-
