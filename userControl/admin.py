06bc3584c75c566365ae45b85714b27059653ee4
Тимур Горащенко <timyr98239718@mail.ru>
2021-09-17 16:28:33 +0300

    AddChangeLog

--- userControl/admin.py
+++ userControl/admin.py
@@ -1,6 +1,45 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.models import LogEntry
  
-from .models import User
+from .models import User, Role
  
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Пользователи"""
+    
+    list_display = ('username', 'last_name', 'first_name','middle_name', 'date_joined', 'last_login')
+    search_fields = ("username", "last_name")
+    list_filter = ("last_login",)
+    fieldsets = (
+        (None, {
+            "fields": (("username",),)
+        }),
+        (None, {
+            "fields": (("last_name", "first_name", "middle_name"),)
+        }),
+        (None, {
+            "fields": (("role",),)
+            
+        }),
+   
+    )
+    
+    
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    """Роли"""
+    list_display = ("name",)
+    
+
+
+ 
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'action_time', 'user', 'content_type', 'object_id', 'object_repr', 'action_flag', 'change_message')
+    list_filter = ('content_type',)
+    search_fields = ['user__username',]
+    date_hierarchy = 'action_time'
+
+    
+admin.site.register(LogEntry, LogEntryAdmin)
