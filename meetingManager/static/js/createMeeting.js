af09b76bc4b758fa489bd487885a842feb8176fb
Тимур Горащенко <timyr98239718@mail.ru>
2021-09-28 14:50:52 +0300

    add icons and add custom table meeting

--- meetingManager/static/js/createMeeting.js
+++ meetingManager/static/js/createMeeting.js
@@ -29,10 +29,17 @@ function addInMeetingList(self, listId, object, objectId){
     var tr = document.createElement("tr");
     if (faucet1 == 1) {
         if (listId == 'researchInMeeting') {
-            tr.innerHTML = '<td id="'+ objectId +'_research1">' + object + '</td> <td id="'+ objectId +'_research2"><button type="button" researchId="'+ objectId + '" class="btn btn-danger" onclick="deleteInMeetingList(this, `research`, '+ objectId + ')">Удалить</button></td>';
+            tr.innerHTML = '<td id="'+ objectId +'_research1">' + object + '</td> <td id="'+ objectId +'_research2"><button type="button" researchId="'+ objectId + '" class="btn" onclick="deleteInMeetingList(this, `research`, '+ objectId + ')">'
+            + '<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-clipboard-minus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
+            +'<path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>'
+            +'<path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3zm-1 9.5A.5.5 0 0 1 6 9h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>'
+            +'</svg></button></td>';
         }
         else {
-            tr.innerHTML = '<td id="'+ objectId +'_user1">' + object + '</td> <td id="'+ objectId +'_user2"><button type="button" class="btn btn-danger" onclick="deleteInMeetingList(this, `user`, '+ objectId + ')">Удалить</button></td>';
+            tr.innerHTML = '<td id="'+ objectId +'_user1">' + object + '</td> <td id="'+ objectId +'_user2"><button type="button" class="btn" onclick="deleteInMeetingList(this, `user`, '+ objectId + ')">'
+            +'<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person-dash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
+            +'<path fill-rule="evenodd" d="M11 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM1.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM6 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm2 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>'
+          + '</svg></button></td>';
         } 
         table.appendChild(tr);
     }
